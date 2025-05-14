82eb848f2717898509643986834893e2bc110872
Edwin Kayang <ekayang@asu.edu>
2025-05-14 12:00:47 -0700

    testing and scripts

--- basic_test.c
+++ basic_test.c
@@ -1,3 +1,17 @@
-int main(){
-    return 35;
+// int main(){
+//     return 35;
+// }
+
+#include <stdio.h>
+
+int fib(int n) {
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n = 10; // Example: Calculate the 10th Fibonacci number
+    printf("Fibonacci number at position %d is %d\n", n, fib(n));
+    return 0;
 }
--- compile.sh
+++ compile.sh
@@ -1,14 +1,17 @@
+
 export LLVM_ROOT=/home/local/ASUAD/ekayang/Documents/LLVM/zeno-llvm/build
 export MUSL_ROOT=/opt/riscv_musl
 export CC=${LLVM_ROOT}/bin/clang
 export LD=${CC}
-export CFLAGS=" --target=riscv64-unknown-linux-gnu \
+export CFLAGS=" --target=riscv64-unknown-elf \
+                -O0 \
                 -march=rv64imazzeno0p1 \
                 -menable-experimental-extensions \
                 -mabi=lp64 -nostdlib \
                 -I${MUSL_ROOT}/include"
 export LDFLAGS=" -static -fuse-ld=${LLVM_ROOT}/bin/ld.lld --sysroot=${MUSL_ROOT} -rtlib=compiler-rt"
-$CC $CFLAGS -O0 -S -o basic_test_2.s basic_test.ll
+$CC $CFLAGS -S -emit-llvm -o basic_test_2.ll basic_test.c
+# $CC $CFLAGS -O0 -S -o basic_test_2.s basic_test_2.ll
 
 # ${LLVM_ROOT}/bin/llvm-objdump --disassemble -S basic_test > basic_test.dump
 
